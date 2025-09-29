e209226a819c6194011af6e27d6d476bf35e9d56
BiletskaKhristina <Khrista.Biletcka7@gmail.com>
2025-09-29 18:20:22 +0300

    Автоматичний коміт 2025-09-29 18:20:11

---
+++ 1_program_/my_first_app.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+name = "Khrystyna"  # своє ім’я
+location = "Lviv"  # своє улюблене місто
+
+print(f"{name} start programming at {datetime.now()}. {location} is the best city!")
